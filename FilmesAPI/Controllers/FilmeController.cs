e4cb4a646e80352ac773769169752bb89300caf5
momojuniior <momo.ju@hotmail.com>
2024-08-09 21:41:40 -0300

    Add project files.

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,22 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Microsoft.AspNetCore.Mvc.Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        public static List<Filme> filmes = new List<Filme>();
+        
+        [HttpPost]
+        public void AdicionarFilme([FromBody]Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+            Console.WriteLine(filme.Duracao);
+            Console.WriteLine(filme.Genero);
+        }
+    }
+}
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        [Required(ErrorMessage = "O título do filme é obrigatório")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O gênero do filme é obrigatório")]
+        [MaxLength(50, ErrorMessage = "O tamanho do gênero não deve exceder 50 caracteres")]
+        public string Genero { get; set; }
+        [Required(ErrorMessage = "O título do filme é obrigatório")]
+        [Range(70, 600, ErrorMessage = "A duração deve ter entre 70 e 600 minutos")]
+        public int Duracao { get; set; }
+    }
+}
